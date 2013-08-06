93d2e7898f6e66eaadc75def2da1c9e8a7ae004a
Fiachna <fiachna.carter@gmail.com>
2013-08-06 19:21:45 +1000

    add inc message to start.clj

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -2,7 +2,7 @@
     (:require [clojure.string :as string]
               [io.pedestal.app.messages :as msg]))
 
-(def inc-transform [old-value _]
+(defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
 (def example-app
